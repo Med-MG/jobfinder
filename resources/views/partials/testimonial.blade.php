4583ee1728122d47384c15403e52b014d310869a
Med Mouiguina <Med@gmail.com>
2019-12-10 14:17:05 +0100

    Initial commit! in this first commit we have all our files and project simi finished there still is some work to be done

---
+++ resources/views/partials/testimonial.blade.php
@@ -0,0 +1,26 @@
+<div class="site-section" data-aos="fade">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 mb-5 mb-md-0">
+            
+              <div class="img-border">
+                
+                 
+                  <span class="icon-wrap">
+                    <span class="icon icon-play"></span>
+                  </span>
+                  <img src="external/images/hero_2.jpg" alt="Image" class="img-fluid rounded">
+                </a>
+              </div>
+            
+          </div>
+          <div class="col-md-5 ml-auto">
+            <div class="text-left mb-5 section-heading">
+              <h2>Testimonies</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   
